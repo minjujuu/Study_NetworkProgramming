b56a80ae237d69e6111a7485e1a6a932545c038e
minjujuu <79794414+minjujuu@users.noreply.github.com>
2022-05-06 00:34:40 +0900

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -12,9 +12,12 @@ int main()
 
     // create pipe channel
     int pipeChannel[2] = {0};
+    int pipeChannel2[2] = {0}; // 파이프는 단방향 통신이기 때문에 부모,자식이 메시지를 주고 받으려면 두 개의 파이프 필요
+
     for (int i = 0; i < 2; i++)
     {
         printf("BEFORE | pipe channel [%d] = %d \n", i, pipeChannel[i]);
+        printf("BEFORE | pipe channel [%d] = %d \n", i, pipeChannel2[i]);
     }
 
     // 아래 코드 이후 pipeChannel에는 3,4가 들어오는데
@@ -24,10 +27,16 @@ int main()
         perror("pipe error");
         exit(1); // 1이면 비정상 종료 (0이면 정상 종료)
     }
+    if (pipe(pipeChannel2) < 0)
+    {
+        perror("pipe error");
+        exit(1); // 1이면 비정상 종료 (0이면 정상 종료)
+    }
 
     for (int i = 0; i < 2; i++)
     {
         printf("AFTER | pipe channel [%d] = %d \n", i, pipeChannel[i]);
+        printf("AFTER | pipe channel [%d] = %d \n", i, pipeChannel2[i]);
     }
 
     childId = fork();
@@ -39,10 +48,20 @@ int main()
         
         char buf[80];
         memset(buf, 0, sizeof(buf)); // clear
+        printf("BEFORE | read to pipeChannel \n");
+        close(pipeChannel[1]); // 1번 채널은 읽기 용도만 쓸 것 
+        // read는 읽을 데이터가 없어야 깨어나므로 누가 먼저 파이프에 쓰느냐느느 상관없음ㅊ 
         read(pipeChannel[0], buf, sizeof(buf));
-        printf("child pipe to read : %s \n", buf);
+        printf("AFTER | read to pipe \n");
+        printf("child pipeChannel[0] to read : %s\n", buf);
+        
+        strcat(buf, "_CHILD");
+        write(pipeChannel2[1], buf, strlen(buf));
+        close(pipeChannel2[0]);
+        printf("child write to pipeChannel2[1] %s\n", buf);
         sleep(2);
         printf("child exit \n");
+    
         exit(0);
     }
     else if (childId < 0)
@@ -60,8 +79,14 @@ int main()
     char temp[80];
     strcpy(temp, "korea");
     write(pipeChannel[1], temp, strlen(temp));
-    printf("parent \"korea\" write to pipe ==> child \n");
+    close(pipeChannel[0]); // parent를 해당 채널을 write용도로만 쓰므로 
 
+    printf("parent \"korea\" write to pipeChannel[1] ==> child \n");
+
+    memset(temp, 0, sizeof(temp));
+    close(pipeChannel2[1]); // 2번 channel은 1번 용도로만 쓰므로
+    read(pipeChannel2[0], temp, sizeof(temp));
+    printf("parent read to pipeChannel2[0]: %s\n", temp);
     sleep(10);
     printf("parent exit \n");
 
@@ -69,21 +94,4 @@ int main()
 
     return 0; // 함수를 콜한 곳으로 돌아감
 
-    /* out
-        parent pid = 178, parent ppid = 8, child id = 179
-        child pid = 179, ppid = 178
-        parent number = 35, global = 23
-        child number = 15, global = 13
-        child exit
-        parent exit
-    */
-
-    /* 결과 정리
-        코드 상에서는 똑같은 num이지만 fork하는 순간 clone 되고,
-        부모와 자식은 서로 다른 주소 공간을 가지게 되기 때문에
-        서로의 number 값에 영향을 줄 수 없음
-
-        전역변수(global) 여도 서로의 값에 영향을 줄 수 없는데,
-        fork 시점에 local, global 변수든 간 새로운 프로그램으로 클론되어 영향을 줄 수 없음
-     */
 }
